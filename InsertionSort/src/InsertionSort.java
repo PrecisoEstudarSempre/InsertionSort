6f60d86f69d8a89746bac7bc497c92f191fa7a25
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-03-10 15:47:48 -0300

    Commit de classe

---
+++ InsertionSort/src/InsertionSort.java
@@ -0,0 +1,33 @@
+class InsertionSort {
+	
+	private void sort(int[] values){
+		int in, out;
+
+		for (out=1; out<values.length; out++) {
+			long temp = values[out];
+			in = out;
+			while(in>0 && values[in-1] >= temp){
+				values[in] = values[in-1];
+				--in;
+			}			
+			values[in] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		InsertionSort insertionSort = new InsertionSort();
+		int size = 10;
+		int[] values = new int[size];
+		int[] values = insertionSort.gerarValoresRandomicos(values,size);
+		insertionSort.sort(values);
+		for (int val : values) {
+			System.out.println(val);
+		}
+	}
+
+	private void gerarValoresRandomicos(int[] array, int size){
+		for (int i=0; i<array.length; i++) {
+			array[i]=(int)(Math.random()*size);
+		}
+	}
+}
