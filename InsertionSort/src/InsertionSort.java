e9c9bce898283bcdc16aebdb6ddc9e9acd184df6
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-03-11 17:42:42 -0300

    Alteração de algoritmo

--- InsertionSort/src/InsertionSort.java
+++ InsertionSort/src/InsertionSort.java
@@ -1,32 +1,42 @@
 class InsertionSort {
 	
 	private void sort(int[] values){
-		int in, out;
+		int i,j;
 
-		for (out=1; out<values.length; out++) {
-			int temp = values[out];
-			in = out;
-			while(in>0 && values[in-1] >= temp){
-				values[in] = values[in-1];
-				--in;
+		for (i=1; i<values.length; i++) {
+			int temp = values[i];
+			j = i;
+			while(j>0 && values[j-1] >= temp){
+				values[j] = values[j-1];
+				--j;
 			}			
-			values[in] = temp;
+			values[j] = temp;
 		}
 	}
 
 	public static void main(String[] args) {
 		InsertionSort insertionSort = new InsertionSort();
-		int size = 10;
+		int size = 10000;
 		int[] values = new int[size];
 		insertionSort.generateRandomValues(values,size);
+		long tempoInicial = System.currentTimeMillis();
 		insertionSort.sort(values);
+		System.out.println("Tempo de execucao do algoritmo: " + (System.currentTimeMillis() - tempoInicial));
+		/*
 		for (int val : values) {
 			System.out.println(val);
+		}
+		*/
+	}
+
+	private void generateSortedValues(int[] array){
+		for (int i=0; i<1000; i++) {
+			array[i] = i;
 		}
 	}
 
 	private void generateRandomValues(int[] array, int size){
-		for (int i=0; i<array.length; i++) {
+		for (int i=1001; i<array.length; i++) {
 			array[i]=(int)(Math.random()*size);
 		}
 	}
